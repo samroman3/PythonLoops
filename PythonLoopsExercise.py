0e28c4e014a8980c68f7f814745ac3c5a214069d
Sam Roman <samroman3@gmail.com>
2021-01-19 10:21:33 -0500

    Adds two list problems

--- PythonLoopsExercise.py
+++ PythonLoopsExercise.py
@@ -1,3 +1,4 @@
+
 def has_lucky_number(nums):
     """Return whether the given list of numbers is lucky. A lucky list contains
     at least one number divisible by 7.
@@ -52,3 +53,39 @@ def elementwise_greater_than(L, thresh):
         if meals[i] == meals[i+1]:
             return True
     return False
+
+
+
+#Example of Monte Carlo Method - need to expand
+# def estimate_average_slot_payout(n_runs):
+#     """Run the slot machine n_runs times and return the average net profit per run.
+#     Example calls (note that return value is nondeterministic!):
+#     >>> estimate_average_slot_payout(1)
+#     -1
+#     >>> estimate_average_slot_payout(1)
+#     0.5
+#     """
+#     avgList = []
+#     for i in range(n_runs):
+#         avgList.append(play_slot_machine()) - 1
+    
+#     return sum(avgList) / len(avgList)
+
+
+party_attendees = ['Adela', 'Fleda', 'Owen', 'May', 'Mona', 'Gilbert', 'Ford']
+def fashionably_late(arrivals, name):
+    """Given an ordered list of arrivals to the party and a name, return whether the guest with that
+    name was fashionably late.
+    """
+    order = arrivals.index(name)
+    return order >= len(arrivals) / 2 and order != len(arrivals) - 1
+
+
+def select_second(L):
+    """Return the second element of the given list. If the list has no second
+    element, return None.
+    """
+    if len(L) < 2:
+        return None    
+    else:
+        return L[1]
