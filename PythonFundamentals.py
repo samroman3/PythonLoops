e90fe4e79ba12a8be491fc27fa61427f3aac6cc8
Sam Roman <samroman3@gmail.com>
2021-01-20 11:04:03 -0500

    adds notes on dictionaries

--- PythonFundamentals.py
+++ PythonFundamentals.py
@@ -147,3 +147,77 @@ datestr = '1956-01-31'
 year, month, day = datestr.split('-')
 print(year,month,day)
 
+# str.join() takes us in the other direction, 
+# sewing a list of strings up into one long string, using the string it was called on as a separator.
+
+print('/'.join([month, day, year]))
+
+print(' 👏 '.join([word.upper() for word in words]))
+
+#concatenation works the same in python as swift by using "+" to join strings
+
+planet + ",we miss you"
+
+position = 9
+planet + ", you'll always be the " + str(position) + "th planet to me."
+
+#Build strings using format() method for cleaner code
+"{}, you'll always be the {}th planet to me.".format(planet, position)
+
+# the Python values we want to insert are represented with {} placeholders, 
+# and .format even converts the int for us
+# you can even preform calculations within .format
+
+pluto_mass = 1.303 * 10**22
+earth_mass = 5.9722 * 10**24
+population = 52910390
+
+# 2 decimal points {:.2}   
+# 3 decimal points, format as percent {:.3%}     
+# separate with commas {:,}
+
+print("""{} weighs about {:.2} kilograms ({:.3%} of Earth's mass). 
+It is home to {:,} Plutonians.""".format(planet, pluto_mass, pluto_mass / earth_mass, population,))
+
+#you can also refer to format() arguments by index, starting from 0
+s = """Pluto's a {0}.
+No, it's a {1}.
+{0}!
+{1}!""".format('planet', 'dwarf planet')
+print(s)
+
+#Dictionaries , built in data structure for mapping keys to values
+numbers = {'one':1, 'two':2, 'three':3}
+# Values are accessed via square bracket syntax similar to indexing into lists and strings.
+print(numbers["one"])
+#adding a new key value pair or changing an existing one is as easy as assigning it:
+numbers["eleven"] = 11
+numbers["three"] = 5
+
+#heres an example of Dictionary Comprehensions with syntax similar to list comprehensions
+planets = ['Mercury', 'Venus', 'Earth', 'Mars', 'Jupiter', 'Saturn', 'Uranus', 'Neptune']
+
+#creates a dictionary using keys from the planet list with their initial as the value
+planet_to_initial = {planet: planet[0] for planet in planets}
+print(planet_to_initial)
+
+# The in operator tells us whether something is a key in the dictionary
+
+print("Saturn" in planet_to_initial)
+
+#for loops will loop over dictionary keys
+for k in numbers:
+    print("{} = {}".format(k, numbers[k]))
+
+#can also a list of all the keys or values with dict.keys or dict.values, respectively
+allKeys = planet_to_initial.keys() 
+print(allKeys)
+
+# Get all the initials, sort them alphabetically, and put them in a space-separated string.
+' '.join(sorted(planet_to_initial.values()))
+
+#dict.items lets you iterate over both keys and values simultaneously 
+# (In Python jargon, an item refers to a key, value pair)
+
+for planet,initial in planet_to_initial.items():
+    print("{} begins with \"{}\"".format(planet,initial))
